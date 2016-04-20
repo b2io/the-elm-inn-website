88008be4e23f5f18ff36de941dc7cd4b70f93bd0
Chris <me@chrishawkins.us>
2016-04-20 17:31:40 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# the-elm-inn-website
+# Website for The Elm Inn 
+
+To get started:
+`npm install`
+
+
+To develop:
+`gulp`
+
+
+To deploy:
+`gulp deploy`
+
+images are on dropbox: b2/client/elm
+
+other account info is on drive: b2/clients/cooperstown
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var surge = require('gulp-surge');
+
+gulp.task('sass', function(){
+  return gulp.src('./scss/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('browserSync', function() {
+  browserSync({
+    server: {
+      baseDir: './'
+    },
+  })
+});
+
+gulp.task('default', ['browserSync', 'sass'], function(){
+  gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./*.html', browserSync.reload);
+  gulp.watch('./js/**/*.js', browserSync.reload);
+});
+
+gulp.task('deploy', [], function() {
+  return surge({
+    project: './',
+    domain: 'theelminn.com'
+  })
+});
