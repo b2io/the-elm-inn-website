4396a8570d45e9cb4964d495f44385fc60568eeb
Chris <me@chrishawkins.us>
2016-05-08 18:02:21 -0400

    feature(build): adds gulp autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,24 @@
+var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync');
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var surge = require('gulp-surge');
 
-gulp.task('sass', function(){
+gulp.task('sass', function() {
   return gulp.src('./scss/*.scss')
     .pipe(sass())
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }))
+});
+
+gulp.task('autoprefix', function() {
+  return gulp.src('./css/styles.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
     .pipe(gulp.dest('./css'))
     .pipe(browserSync.reload({
       stream: true
@@ -20,7 +33,7 @@ gulp.task('browserSync', function() {
   })
 });
 
-gulp.task('default', ['browserSync', 'sass'], function(){
+gulp.task('default', ['browserSync', 'sass', 'autoprefix'], function() {
   gulp.watch('./scss/**/*.scss', ['sass']);
   gulp.watch('./*.html', browserSync.reload);
   gulp.watch('./js/**/*.js', browserSync.reload);
